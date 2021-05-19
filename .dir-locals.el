e31af44cfd4558d67e4a78cf4728fb62307776ef
Paul Szenher <paul@szenher.com>
2021-05-18 21:15:45 -0400

    Migrate dockerfile apt pinning code into separate repo
    Update apt-version-pin github actions workflow to use external package

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Emacs Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (eval add-hook 'before-save-hook #'lsp-format-buffer nil t)))
